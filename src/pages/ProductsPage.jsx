d619af8aab7c55f8fb58e8430a85c0e82f3841ea
azam golestan <goli.goli499a.com>
2024-04-02 21:11:47 +0330

    Show Products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,20 @@
 import { useProducts } from "../context/ProductContext";
+import styles from "./ProductPage.module.css"
 
 function ProductsPage(props) {
   const products = useProducts();
   console.log(products);
-  return <div>ProductsPage</div>;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.product}>
+        {products.map((p) => (
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>SideBar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
