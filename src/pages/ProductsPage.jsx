84bdf28abfa67ad316342c7a2f9299693d352d50
azam golestan <goli.goli499a.com>
2024-04-02 23:52:29 +0330

    Created Search Section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
+
 import styles from "./ProductPage.module.css";
 
 function ProductsPage(props) {
   const products = useProducts();
 
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log("Search");
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.product}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>SideBar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.product}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>SideBar</div>
+      </div>
+    </>
   );
 }
 
