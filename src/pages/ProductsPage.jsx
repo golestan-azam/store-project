64b05a7a62592352a37491dc64cb6653eb540dbe
azam golestan <goli.goli499a.com>
2024-04-05 23:23:14 +0330

    Multi-Stage Filter

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -6,6 +6,7 @@ import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 
+import { filterProducts, searchProducts } from "../helper/helper";
 import styles from "./ProductPage.module.css";
 
 function ProductsPage(props) {
@@ -20,7 +21,9 @@ function ProductsPage(props) {
   }, [products]);
 
   useEffect(() => {
-    console.log(query);
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
   }, [query]);
 
   const searchHandler = () => {
