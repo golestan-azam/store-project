b50d44bd4a2197af2e5972ed6de78c6028b2776a
azam golestan <goli.goli499a.com>
2024-04-05 18:54:57 +0330

    Create Category Section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 
 import Card from "../components/Card";
 import Loader from "../components/Loader";
@@ -15,6 +16,16 @@ function ProductsPage(props) {
   const searchHandler = () => {
     console.log("Search");
   };
+
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+
+    if (tagName !== "LI") return;
+
+    console.log(category);
+  };
+
   return (
     <>
       <div>
@@ -35,7 +46,19 @@ function ProductsPage(props) {
             <Card key={p.id} data={p} />
           ))}
         </div>
-        <div>SideBar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
