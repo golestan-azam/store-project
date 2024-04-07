c1c0326874f1a1ebe2d164d2fb818bfac807333b
azam golestan <goli.goli499a.com>
2024-04-07 19:46:17 +0330

    Combination Context and useReducer for CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
 import ProductsProvider from "./context/ProductContext";
+import CartProvider from "./context/CartContext";
 
 function App() {
   return (
-    <ProductsProvider>
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/products" replace />} /> */}
-        <Route index element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </ProductsProvider>
+    <CartProvider>
+      <ProductsProvider>
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/products" replace />} /> */}
+          <Route index element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </ProductsProvider>
+    </CartProvider>
   );
 }
 
