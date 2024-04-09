6f5cc5d37d26144636768e6260704e3d110381e8
azam golestan <goli.goli499a.com>
2024-04-09 19:30:08 +0330

    Launch the Shopping Cart Page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,10 +1,24 @@
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartContext";
 
-function CheckoutPage(props) {
-    return (
-        <div>
-            CheckoutPage
-        </div>
-    );
+function CheckoutPage() {
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
+  return (
+    <div>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutPage;
