fc997487d2f718ba85f97b84dc70df90f5f9f8e3
azam golestan <goli.goli499a.com>
2024-04-09 18:26:58 +0330

    Launch the Product Details Page

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -32,6 +32,12 @@ const useProducts = () => {
   return products;
 }; // custom hook
 
+const useProductDetails = (id) => {
+  const products = useContext(ProductContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
+
 export default ProductsProvider;
 
-export { useProducts };
+export { useProducts, useProductDetails };
