832e516b741013182d6c1dc5a8328b4044f980f8
azam golestan <goli.goli499a.com>
2024-04-10 01:06:37 +0330

    Complete Sidebar in Basket Page

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -5,11 +5,12 @@ import { shortenText } from "../helper/helper";
 import styles from "./BasketCart.module.css";
 
 function BasketCard({ data, clickHandler }) {
-    const {image, title, quantity} = data
+  const { image, title, quantity, price } = data;
   return (
     <div className={styles.card}>
       <img src={image} alt={title} />
       <p>{shortenText(title)}</p>
+      <p>{(quantity * price).toFixed(2)} $</p>
       <div className={styles.actions}>
         {quantity === 1 && (
           <button onClick={() => clickHandler("REMOVE_ITEM", data)}>
